452f07e1cfd8673ea522dc0735cc12374c86d516
Najib-Hebrawi <71656543+Najib2020-Hebrawi@users.noreply.github.com>
2021-11-29 10:17:59 +0100

    First Version 1.3 : Add PlayingGame logic

--- app/src/main/java/com/mhebrawi/s181663/PlayingGameFragment.kt
+++ app/src/main/java/com/mhebrawi/s181663/PlayingGameFragment.kt
@@ -1,10 +1,16 @@
 package com.mhebrawi.s181663
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.mhebrawi.s181663.databinding.FragmentPlayingGameBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +23,16 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class PlayingGameFragment : Fragment() {
+
+
+    var lives: Int = 5
+    var scores: Int = 0
+    var points: Int = 0
+    var handler = Handler()
+
+
+    private lateinit var binding: FragmentPlayingGameBinding
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,23 +53,186 @@ class PlayingGameFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_playing_game, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PlayingGameFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PlayingGameFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentPlayingGameBinding.bind(view)
+        setLife(lives)
+        setScore(scores)
+        updateLifeScore()
+        binding.textViewShowTheWord.text = getRandomLandName()
+        binding.textViewShowTheWord.text = hiddenTheWord()
+
+        binding.buClickToSendTheLetter.setOnClickListener {
+            game()
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        spin()
+    }
+
+
+    fun spinAgain() {
+        binding.apply {
+            buClickToSendTheLetter.visibility = View.GONE
+            EditTextWritTheWord.visibility = View.GONE
+            EditTextWritTheWord.setText("")
+            buClickToStartSpining.visibility = View.VISIBLE
+        }
+    }
+
+    @SuppressLint("WrongConstant")
+    fun spin() {
+        binding.apply {
+            buClickToStartSpining.setOnClickListener {
+                buClickToStartSpining.visibility = View.GONE
+                progressBar.visibility = View.VISIBLE
+                spinWheelFun()
+                spinlogic()
+                if (getSpinner() in 0..2) {
+                    val x = Runnable {
+                        progressBar.visibility = View.GONE
+                        buClickToSendTheLetter.visibility = View.VISIBLE
+                        textViewShowTheWord.visibility = View.VISIBLE
+                        EditTextWritTheWord.visibility = View.VISIBLE
+                        buClickToStartSpining.visibility = View.GONE
+                        Toast.makeText(activity, getSpinnerResult(), 0).show()
+                    }
+                    handler.postDelayed(x, 1000)
+                } else if (getSpinner() == 3) {
+                    progressBar.visibility = View.VISIBLE
+                    Toast.makeText(activity, getSpinnerResult() + " prøv igen ", 0).show()
+                    buClickToStartSpining.visibility = View.VISIBLE
+                    textViewShowTheWord.visibility = View.GONE
+                    EditTextWritTheWord.visibility = View.GONE
+                    progressBar.visibility = View.GONE
                 }
             }
+        }
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    fun spinlogic() {
+
+        binding.apply {
+            when {
+                getSpinner() == 0 -> {
+                    extra1000(scores)
+                    scores = getScore()
+                    binding.playerScore.text = "score$scores"
+                    buClickToSendTheLetter.setOnClickListener {
+                        game()
+                    }
+                }
+                getSpinner() == 1 -> {
+                    extraLife(lives)
+                    lives = updateLives()
+                    binding.playerLives.text = "Lives $lives"
+                    buClickToSendTheLetter.setOnClickListener {
+                        game()
+                    }
+                }
+                getSpinner() == 2 -> {
+                    resetScore(scores)
+                    scores = getScore()
+                    binding.playerScore.text = "Score$scores"
+                    buClickToSendTheLetter.setOnClickListener {
+                        game()
+                    }
+                }
+                getSpinner() == 3 -> {
+                    minusLife(lives)
+                    lives = updateLives()
+                    binding.playerLives.text = "lives $lives"
+                    buClickToStartSpining.visibility = View.VISIBLE
+
+
+                }
+                lives <= 0 -> {
+                    Toast.makeText(activity, "Du har tabte", Toast.LENGTH_LONG).show()
+                    gamWonOver()
+
+                }
+            }
+
+
+        }
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    private fun updateLifeScore() {
+        binding.apply {
+            playerLives.text = "Lives : ${updateLives()}"
+            playerScore.text = "Score : ${getScore()}"
+        }
+    }
+
+
+    @SuppressLint("WrongConstant", "SetTextI18n")
+    fun game() {
+        binding.apply {
+            if (EditTextWritTheWord.text.trim().toString().isNotEmpty()) {
+                if (findLetterAsGusset(EditTextWritTheWord.text.toString())) {
+                    Toast.makeText(activity, getMessage(), 1).show()
+                    points = getCounter() * 1000
+                    scores += points
+                    updateScore()
+                    textViewShowTheWord.text = updateTheHiddenWord()
+                    playerScore.text = "Score  : " + getScore() + "DKK"
+                    resetCounter()
+                    if (!isWon())
+                        Toast.makeText(activity, " Du Har vundet", Toast.LENGTH_LONG).show()
+                    else {
+                        spinAgain()
+                    }
+                } else if (!findLetterAsGusset(EditTextWritTheWord.text.toString()) && lives == 1) {
+                    minusLife(lives)
+                    lives = updateLives()
+                    binding.playerLives.text = "Lives : $lives"
+                    buClickToStartSpining.visibility = View.VISIBLE
+                    Toast.makeText(activity, getSpinnerResult(), 0).show()
+                    buClickToSendTheLetter.visibility = View.GONE
+                    EditTextWritTheWord.visibility = View.GONE
+                    Toast.makeText(activity, "Du har tabte", Toast.LENGTH_LONG).show()
+                    gamWonOver()
+
+
+                } else if (!findLetterAsGusset(EditTextWritTheWord.text.toString())) {
+                    minusLife(lives)
+                    lives = updateLives()
+                    binding.playerLives.text = "Lives$lives"
+                    buClickToStartSpining.visibility = View.VISIBLE
+                    spinAgain()
+                } else if (isWon() && getScore() < 0) {
+                    TODO() // her jeg skal implemntere at man vinder
+                }
+            }
+        }
+    }
+
+
+    fun gamWonOver() {
+        val builder = AlertDialog.Builder(requireContext())
+        with(builder) {
+            setTitle("   ")
+            setPositiveButton("spil igen") { dialog, which ->
+                requireActivity().onBackPressed()
+
+            }
+            setNegativeButton("luk spilet") { dialog, which ->
+
+
+                Log.d("Main", "Negative button clicked")
+            }
+            show()
+        }
+
     }
 }
+
+
